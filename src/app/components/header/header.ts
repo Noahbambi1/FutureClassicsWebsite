4b4230b2049ae2f0bdb0eed2d6fac40a14a285f1
NoahHabanero <noah@habanerosystems.com>
2025-12-12 12:23:03 +0200

    make mobile friendly

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,6 +1,6 @@
-import { Component, signal, HostListener, effect, PLATFORM_ID, Inject } from '@angular/core';
+import { Component, signal, HostListener } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
@@ -11,34 +11,10 @@ import { CommonModule, isPlatformBrowser } from '@angular/common';
 export class Header {
   isScrolled = signal(false);
   isMobileMenuOpen = signal(false);
-  private isBrowser: boolean;
-
-  constructor(@Inject(PLATFORM_ID) platformId: Object) {
-    this.isBrowser = isPlatformBrowser(platformId);
-    
-    // Effect to control body scroll when menu opens/closes
-    effect(() => {
-      if (this.isBrowser) {
-        if (this.isMobileMenuOpen()) {
-          document.body.classList.add('menu-open');
-        } else {
-          document.body.classList.remove('menu-open');
-        }
-      }
-    });
-  }
 
   @HostListener('window:scroll')
   onScroll() {
     this.isScrolled.set(window.scrollY > 50);
-  }
-
-  @HostListener('window:resize')
-  onResize() {
-    // Close mobile menu on resize to desktop
-    if (this.isBrowser && window.innerWidth > 768 && this.isMobileMenuOpen()) {
-      this.closeMobileMenu();
-    }
   }
 
   toggleMobileMenu() {
